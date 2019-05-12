0503295bee48478082de0231337901d168c18c4d
Matt Policane <mattpolicane@gmail.com>
2019-05-12 11:43:17 +0100

    update links

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -6,8 +6,8 @@ import IconTemplate from './templates/IconTemplate';
 const Icons = () => {
   return (
     <div className="Icons">
-      <IconTemplate href="https://twitter.com/justcalledmatt" icon="fa-twitter fa-3x" />
-      <IconTemplate href="https://www.facebook.com/matt.policane" icon="fa-facebook fa-3x" />
+      <IconTemplate href="https://twitter.com/MPolicane" icon="fa-twitter fa-3x" />
+      <IconTemplate href="https://www.facebook.com/policane" icon="fa-facebook fa-3x" />
       <IconTemplate href="https://www.instagram.com/mattpolicane/" icon="fa-instagram fa-3x" />
       <IconTemplate href="https://www.linkedin.com/in/matt-policane-5395289b?trk=nav_responsive_tab_profile_pic" icon="fa-linkedin fa-3x" />
       <IconTemplate href="https://github.com/officialmatt" icon="fa-github fa-3x" />
